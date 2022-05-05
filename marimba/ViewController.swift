af8e836ff110d45d020c3d204ad85bde8e4f07f6
carlos montes <chuckmountain71@gmail.com>
2022-05-05 11:09:25 -0500

    primer commit

--- marimba/ViewController.swift
+++ marimba/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var reproductor: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func teclaSeleccionadaBTN(_ sender: UIButton) {
+        
+        print(sender.titleLabel?.text ?? "1")
+        
+        reproducirNota(cualNota: sender.titleLabel?.text ?? "1")
+        
+    }
+    
+    func reproducirNota(cualNota: String){
+        guard let url = Bundle.main.url(forResource: cualNota, withExtension: "mp3") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            reproductor = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            reproductor?.play()
+            
+        } catch let error {
+            
+            print(error.localizedDescription)
+            
+        }
+        
+    }
+    
+    
 }
 
